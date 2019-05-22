8709bedb92c8a6e0872f3d5c79006da97804f13e
Alex Reed <ch.alexandre.reed@gmail.com>
2019-05-22 14:56:12 +0200

    its good

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, except: [:edit, :update, :destroy] do
+    resources :reviews, only: [ :index, :new, :create ]
+  end
+  resources :reviews, only: [ :show ]
+
+  root to: 'restaurants#index'
 end
